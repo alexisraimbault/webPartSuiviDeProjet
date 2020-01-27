bb6419c115de8b47485b78ddabe4cc1e1b56fc93
alexisraimbault <42438383+alexisraimbault@users.noreply.github.com>
2020-01-27 19:20:59 +0100

    modal to show desc and comments

--- src/webparts/pistl/components/Artefact.tsx
+++ src/webparts/pistl/components/Artefact.tsx
@@ -4,20 +4,82 @@ import {IArtefactProps} from './IArtefactProps';
 import CommentList from './CommentList';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { Dropdown, DropdownMenuItemType } from 'office-ui-fabric-react/lib/Dropdown';
+import {
+  getTheme,
+  mergeStyleSets,
+  FontWeights,
+  ContextualMenu,
+  Checkbox,
+  DefaultButton,
+  Modal,
+  IDragOptions,
+  IconButton
+} from 'office-ui-fabric-react';
 import { Button} from 'office-ui-fabric-react/lib/Button';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { Label } from 'office-ui-fabric-react/lib/Label';
+
+
 import Popup from './Popup'
 
 const logo = require('../user.png');
+const theme = getTheme();
+const contentStyles = mergeStyleSets({
+  container: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    alignItems: 'stretch'
+  },
+  header: [
+    theme.fonts.xLargePlus,
+    {
+      flex: '1 1 auto',
+      borderTop: `4px solid ${theme.palette.themePrimary}`,
+      color: theme.palette.neutralPrimary,
+      display: 'flex',
+      alignItems: 'center',
+      fontWeight: FontWeights.semibold,
+      padding: '12px 12px 14px 24px'
+    }
+  ],
+  body: {
+    flex: '4 4 auto',
+    padding: '0 24px 24px 24px',
+    overflowY: 'hidden',
+    selectors: {
+      p: {
+        margin: '14px 0'
+      },
+      'p:first-child': {
+        marginTop: 0
+      },
+      'p:last-child': {
+        marginBottom: 0
+      }
+    }
+  }
+});
 
-export default class Artefact extends React.Component<IArtefactProps, {showPopup : boolean, selectState:string, oldState:string, newCommentText : string}> {
+const iconButtonStyles = mergeStyleSets({
+  root: {
+    color: theme.palette.neutralPrimary,
+    marginLeft: 'auto',
+    marginTop: '4px',
+    marginRight: '2px'
+  },
+  rootHovered: {
+    color: theme.palette.neutralDark
+  }
+});
+
+export default class Artefact extends React.Component<IArtefactProps, {showModal:boolean, showPopup : boolean, selectState:string, oldState:string, newCommentText : string}> {
 
   
   constructor(props)
   {  
     super(props);  
     this.state = {showPopup: false,
+                  showModal:false,
                   selectState : escape(this.props.state),
                   oldState :  escape(this.props.state), 
                   newCommentText : ''};  
@@ -25,7 +87,17 @@ export default class Artefact extends React.Component<IArtefactProps, {showPopup
     this.handleChange = this.handleChange.bind(this);
     this.changeInputComment = this.changeInputComment.bind(this);
     this.addComment = this.addComment.bind(this);
+    this._showModal = this._showModal.bind(this);
+    this._closeModal = this._closeModal.bind(this);
   }  
+
+  private _showModal = (): void => {
+    this.setState({ showModal: true });
+  };
+
+  private _closeModal = (): void => {
+    this.setState({ showModal: false });
+  };
 
   addComment()
   {
@@ -83,7 +155,29 @@ export default class Artefact extends React.Component<IArtefactProps, {showPopup
                 <img className={ styles.artefactAuthorIcon } src = {require('../user.png')}></img>
                 <div className={ styles.artefactAuthor }>{this.props.author}</div>
               </div>
-              <div className={ styles.artefactAuthor }>{this.props.desc}</div>
+              <div className={ styles.artefactDescMarginBottom  } onClick={this._showModal}>{this.props.desc.substr(0,5) + '[...]'}</div>
+
+              <Modal
+                isOpen={this.state.showModal}
+                onDismiss={this._closeModal}
+                containerClassName={styles.container}
+                isBlocking={false}
+              >
+                <div className={contentStyles.header}>
+                  <span >Description</span>
+                  <IconButton
+                    styles={iconButtonStyles}
+                    iconProps={{ iconName: 'Cancel' }}
+                    ariaLabel="Close popup modal"
+                    onClick={this._closeModal as any}
+                  />
+                </div>
+                <div  className={contentStyles.body}>
+                  <p>
+                  {this.props.desc}
+                  </p>
+                </div>
+              </Modal>
 
               <Dropdown 
               className={ styles.artefactDropDown }
@@ -118,8 +212,29 @@ export default class Artefact extends React.Component<IArtefactProps, {showPopup
                 <img className={ styles.artefactAuthorIcon } src = {require('../user.png')}></img>
                 <div className={ styles.artefactAuthor }>{this.props.author}</div>
               </div>
-              <div className={ styles.artefactAuthor }>{this.props.desc}</div>
+              <div className={ styles.artefactDesc } onClick={this._showModal}>{this.props.desc.substr(0,5) + '[...]'}</div>
 
+              <Modal
+                isOpen={this.state.showModal}
+                onDismiss={this._closeModal}
+                containerClassName={styles.container}
+                isBlocking={false}
+              >
+                <div className={contentStyles.header}>
+                  <span >Description</span>
+                  <IconButton
+                    styles={iconButtonStyles}
+                    iconProps={{ iconName: 'Cancel' }}
+                    ariaLabel="Close popup modal"
+                    onClick={this._closeModal as any}
+                  />
+                </div>
+                <div  className={contentStyles.body}>
+                  <p>
+                  {this.props.desc}
+                  </p>
+                </div>
+              </Modal>
               <CommentList comments = {this.props.comments}/>
               <div className={styles.center_margin_top}> 
                 <Button text='Edit...' onClick={this.togglePopup.bind(this)} /> 
--- src/webparts/pistl/components/Comment.tsx
+++ src/webparts/pistl/components/Comment.tsx
@@ -2,14 +2,92 @@ import * as React from 'react';
 import {ICommentProps} from './ICommentProps';
 import styles from './Pistl.module.scss';
 
+import {
+  getTheme,
+  mergeStyleSets,
+  FontWeights,
+  ContextualMenu,
+  Checkbox,
+  DefaultButton,
+  Modal,
+  IDragOptions,
+  IconButton
+} from 'office-ui-fabric-react';
+
 const logo = require('../user.png');
 
-export default class Comment extends React.Component<ICommentProps, {}> {
+const theme = getTheme();
+
+const contentStyles = mergeStyleSets({
+  container: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    alignItems: 'stretch'
+  },
+  header: [
+    theme.fonts.xLargePlus,
+    {
+      flex: '1 1 auto',
+      borderTop: `4px solid ${theme.palette.themePrimary}`,
+      color: theme.palette.neutralPrimary,
+      display: 'flex',
+      alignItems: 'center',
+      fontWeight: FontWeights.semibold,
+      padding: '12px 12px 14px 24px'
+    }
+  ],
+  body: {
+    flex: '4 4 auto',
+    padding: '0 24px 24px 24px',
+    overflowY: 'hidden',
+    selectors: {
+      p: {
+        margin: '14px 0'
+      },
+      'p:first-child': {
+        marginTop: 0
+      },
+      'p:last-child': {
+        marginBottom: 0
+      }
+    }
+  }
+});
+
+const iconButtonStyles = mergeStyleSets({
+  root: {
+    color: theme.palette.neutralPrimary,
+    marginLeft: 'auto',
+    marginTop: '4px',
+    marginRight: '2px'
+  },
+  rootHovered: {
+    color: theme.palette.neutralDark
+  }
+});
+
+export default class Comment extends React.Component<ICommentProps, {showModal:boolean}> {
+
+  
     constructor(props)
     {  
       super(props);  
-    } 
 
+      this.state = {
+        showModal:false
+      }
+      
+      this._showModal = this._showModal.bind(this);
+      this._closeModal = this._closeModal.bind(this);
+    }  
+  
+    private _showModal = (): void => {
+      this.setState({ showModal: true });
+    };
+  
+    private _closeModal = (): void => {
+      this.setState({ showModal: false });
+    };
     public render(): React.ReactElement<ICommentProps> 
     {
       return (
@@ -18,7 +96,30 @@ export default class Comment extends React.Component<ICommentProps, {}> {
                 <img className={ styles.artefactAuthorIcon } src = {require('../user.png')}></img>
                 <div className={ styles.artefactAuthor }>{this.props.author}</div>
               </div>
-              <div className={ styles.artefactAuthor }>{this.props.text}</div>
+
+              <div className={ styles.artefactDescMarginBottom  } onClick={this._showModal}>{this.props.text.substr(0,5) + '[...]'}</div>
+
+              <Modal
+                isOpen={this.state.showModal}
+                onDismiss={this._closeModal}
+                containerClassName={styles.container}
+                isBlocking={false}
+              >
+                <div className={contentStyles.header}>
+                  <span >Comment</span>
+                  <IconButton
+                    styles={iconButtonStyles}
+                    iconProps={{ iconName: 'Cancel' }}
+                    ariaLabel="Close popup modal"
+                    onClick={this._closeModal as any}
+                  />
+                </div>
+                <div  className={contentStyles.body}>
+                  <p>
+                  {this.props.text}
+                  </p>
+                </div>
+              </Modal>
           </div>
       );
     }
